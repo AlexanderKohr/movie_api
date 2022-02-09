b8d718af932b7049b42615845520526c0f179737
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-02-09 17:47:07 +0100

    Update passport.js
    Add hash on any password when the user logs in

--- passport.js
+++ passport.js
@@ -22,6 +22,12 @@ passport.use(new LocalStrategy ({
             console.log('incorrect username');
             return callback(null, false, {message: 'Incorrect username or password'});
         }
+        /* hashes any password enteres by the user when logging in before 
+           comparing it to the password stored in MongoDB */
+        if (!user.validatePassword(password)) {
+            console.log('incorrect password');
+            return callback(null, false, {message: 'Incorrect password.'});
+        }
 
         console.log('finished');
         return callback(null, user);
