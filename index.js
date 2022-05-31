ffa0cc04a059d7129d15700f1dbc996e18b1cb4b
AlexanderKohr <94537064+AlexanderKohr@users.noreply.github.com>
2022-05-31 15:25:38 +0200

    Add allowed origin

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ let allowedOrigins = [
     'http://localhost:1234',
     'http://localhost:4200',
     'https://alexanderkohr.github.io',
+    'https://effulgent-chimera-c75f27.netlify.app/'
 ];
 
 /**
